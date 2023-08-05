b0df6f387c76731d6a226241bda93b24c43e5255
germynic31 <geradeev57@gmail.com>
2023-08-05 15:29:42 +0300

    make delete & update news

--- webdj/news/views.py
+++ webdj/news/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news'
+    template_name = 'news/news_delete.html'
 
 
 def news_home(request):
